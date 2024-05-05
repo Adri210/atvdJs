f3135de4a646301696a49719146b26949db44eff
Adriani Delfino <141968057+Adri210@users.noreply.github.com>
2024-05-04 21:40:39 -0300

    commit

--- atvd6.js
+++ atvd6.js
@@ -7,7 +7,7 @@ const matriz = [
 
 function calcularMediaMatriz(matriz) {
   let soma = 0;
-  let elementos = 0;
+  let elementos = 0;0
   for (let i = 0; i < matriz.length; i++) {
       for (let j = 0; j < matriz[i].length; j++) {
           soma += matriz[i][j];
